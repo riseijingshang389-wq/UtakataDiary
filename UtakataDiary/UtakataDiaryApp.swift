30a4f4d7138cc26bc153070d95b9734f3a94022a
Inoue <riseijingshang389@gmail.com>
2026-05-24 15:49:33 +0900

    Initial Commit

---
+++ UtakataDiary/UtakataDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtakataDiaryApp.swift
+//  UtakataDiary
+//
+//  Created by 井上凜清 on 2026/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UtakataDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
